0a97d4c34bcf20263e9c8beeaac73b45865453e7
LBHTKarki <58432863+LBHTKarki@users.noreply.github.com>
2020-07-06 12:10:46 +0100

    update handler

--- api/test.js
+++
@@ -1,6 +0,0 @@
-//dummy endpoint for testing CircleCi
-export default (req, res) => {
-    res.statuscode = 200
-    res.setHeader = ('content-type','application/json')
-    res.end(JSON.stringify({result: 'sample end point'}))
-}

4ec0901c3ce81a7f46c25683ce95983a886b13ba
Tony Griffin <tony@madetech.com>
2020-07-28 07:49:29 +0100

    sv-105/Add simple Cypress tests and Cypress config files to .env

---
+++ cypress/integration/workTrayPage/workTrayPage.spec.js
@@ -0,0 +1,17 @@
+describe('The Work Tray Page', () => {
+  it('successfully loads', () => {
+    cy.visit('/');
+  });
+});
+
+describe('Work Tray Page Elements', () => {
+  it('', () => {
+    cy.visit('/');
+
+    cy.contains("In Progress")
+    cy.contains('Completed');
+    cy.contains('All Items');
+    
+   
+  });
+});

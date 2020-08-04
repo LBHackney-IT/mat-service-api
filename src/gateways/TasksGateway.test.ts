5010f11fbfe6fe3644537b0a55a83372dd36b25b
Tony Griffin <tony@madetech.com>
2020-08-04 16:43:19 +0100

    Add Taskgateway and tests

---
+++ src/gateways/TasksGateway.test.ts
@@ -0,0 +1,41 @@
+import TasksGateway from './TasksGateway';
+import axios from 'axios';
+import { JsonWebTokenError } from 'jsonwebtoken';
+jest.mock('axios');
+
+describe('TasksGateway', () => {
+  beforeEach(() => {
+    axios.mockClear();
+  });
+
+  describe('Get Tasks', () => {
+    it('successfully fetches data from an API', async () => {
+      const data = {
+        body: '',
+        error: undefined,
+      };
+
+      axios.get.mockResolvedValue(data);
+
+      const tasksGateway = new TasksGateway();
+      const response = await tasksGateway.getTasks();
+
+      expect(response).toEqual(data);
+    });
+
+    it('returns an human readable error when unsuccessful', async () => {
+      const errorMessage = 'Network Error';
+      const errorResponse = {
+        body: undefined,
+        error: errorMessage,
+      };
+
+      axios.get.mockReturnValue(Promise.reject(new Error(errorMessage)));
+
+      const tasksGateway = new TasksGateway();
+      const response = await tasksGateway.getTasks();
+
+      expect(response).toEqual(errorResponse);
+    });
+  });
+});
--- src/gateways/TasksGateway.ts
+++ src/gateways/TasksGateway.ts
@@ -1,19 +1,35 @@
+import axios, { AxiosResponse } from 'axios';
+
 interface GetTasksResponse {
-  body: string | undefined
-  error: string | undefined
+  body: string | undefined;
+  error: string | undefined;
+}
+
+interface Error {
+  message: string;
 }
 
 export interface TasksGatewayInterface {
-  getTasks(): GetTasksResponse;
+  getTasks(): Promise<AxiosResponse<GetTasksResponse>>;
 }
 
 class TasksGateway implements TasksGatewayInterface {
-  public getTasks() :GetTasksResponse {
-    return {
-      body: "",
-      error: undefined
-    }
+  public async getTasks(): Promise<AxiosResponse<GetTasksResponse>> {
+    const response = await axios
+      .get('')
+      .then((response) => {
+        const data = response as AxiosResponse<GetTasksResponse>;
+        return data;
+      })
+      .catch((error: Error) => {
+        console.log(error);
+        return {
+          body: undefined,
+          error: error.message,
+        };
+      });
+    return response;
   }
 }
 
-export default TasksGateway
+export default TasksGateway;

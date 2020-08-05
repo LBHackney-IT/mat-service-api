d9ec7240a5e9d24bb5567f912c4cd077a29d9402
Christopher Parsons <mail@christopherparsons.me>
2020-08-05 11:52:34 +0100

    Add Task interface based upon swagger spec

---
+++ src/interfaces/task.ts
@@ -0,0 +1,24 @@
+export interface Task {
+  id: string,
+  createdTime: Date,
+  category: string
+  type: string
+  resident: {
+    presentationName: string,
+    role: string,
+  },
+  address: {
+    presentationShort: string
+  },
+  dueTime: Date,
+  stage: Stage,
+  children?: Task[],
+  parent?: Task
+}
+
+export enum Stage {
+  unstarted,
+  started,
+  validating,
+  completed
+}

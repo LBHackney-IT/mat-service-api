27ee82e1f882e1b5aa4ed3e2a70b9d1742173771
Christopher Parsons <mail@christopherparsons.me>
2020-08-13 11:16:24 +0100

    Add test to check correct response is received without env var

--- src/usecases/ui/getTasks.test.ts
+++ src/usecases/ui/getTasks.test.ts
@@ -9,6 +9,12 @@ describe("getTasks", () => {
     axios.mockClear();
   })
 
+  it("returns an empty array when no env var is set", async () => {
+    const response = await getTasks();
+
+    expect(response).toEqual([]);
+  })
+
   it('successfully fetches data from an API', async () => {
     process.env.NEXT_PUBLIC_API_PATH = "http://localhost:3000/api"
 

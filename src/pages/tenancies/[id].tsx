a3d1f001071302b3bf1dc558d34b34a306f8b445
Tony Griffin <tony@madetech.com>
2020-08-19 15:46:20 +0100

    sv-19/Add page layout

--- src/pages/tenancies/[id].tsx
+++ src/pages/tenancies/[id].tsx
@@ -10,21 +10,23 @@ import {
   NavigationBar,
 } from 'lbh-frontend-react';
 
+const TenancyPage = () => {
   return (
     <Layout>
       <Heading level={HeadingLevels.H2}>1 Hill Street N16 5TT</Heading>
       <Heading level={HeadingLevels.H3}>Tenancy</Heading>
-      <Heading level={HeadingLevels.H3}>Area and Patch</Heading>
+      <Paragraph>Tenancy type: secure</Paragraph>
+      <Paragraph>Tenancy start date: 01/04/1992</Paragraph>
+      <Paragraph>Tenancy reference: 012345678</Paragraph>
 
       <Heading level={HeadingLevels.H3}>Residents</Heading>
       <div>Mrs Joan Fisher</div>
       <div>Mrs Sally Fisher</div>
-      <Button>Edit Contact Details</Button>
-      <Heading level={HeadingLevels.H3}>Notifications</Heading>
-      <div>Name</div>
-      <Heading level={HeadingLevels.H3}>Household members</Heading>
-      <Heading level={HeadingLevels.H3}>Other information</Heading>
-      <Button>Create process</Button>
+      <Paragraph>Date of birth:</Paragraph>
+      <Paragraph>Mobile:</Paragraph>
+      <Paragraph>Home:</Paragraph>
+      <Paragraph>Work:</Paragraph>
+      <Paragraph>Email:</Paragraph>
     </Layout>
   );
 };

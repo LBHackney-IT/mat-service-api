564f8a5c55b2ab9c3a219f0c5fcfc781d022cd95
LBHTKarki <58432863+LBHTKarki@users.noreply.github.com>
2020-08-25 14:36:41 +0100

    merge prep

--- src/gateways/matPostgresGateway.ts
+++ src/gateways/matPostgresGateway.ts
@@ -1,44 +1,7 @@
 import { TRAPatchMappingDBResponseInterface } from '../mappings/apiTRAToUiTRA'
 
 class MatPostgresGateway {
-  public async getTrasByPatchId(patchId: string) {
-    let instance;
-    if (!instance) {
-      const { default: pgp } = await import('pg-promise');
-      let options = {
-        connectionString: process.env.TEST_DATABASE_URL 
-      };
-      if (process.env.NODE_ENV === 'production') {
-        options.connectionString = process.env.DATABASE_URL;
-      }
-      instance = pgp()(options);
-      delete instance.constructor;
-    }
-
-    try {
-      const dbQuery = `
-      SELECT	TRA.Name,
-              TRA.TraId,
-              TRAPatchAssociation.PatchCRMId
-      FROM	TRA INNER JOIN
-              TRAPatchAssociation ON TRA.TRAId = TRAPatchAssociation.TRAId
-      WHERE TRAPatchAssociation.PatchCRMId ='${patchId}'
-      `;
-    
-      const results = await instance.many(dbQuery);
-
-      return Promise.resolve({
-        body: results,
-        error: undefined
-      })
-    }
-    catch(error) {
-      return Promise.resolve({
-        body: undefined,
-        error: 500
-      })
-    }
-  }
+  
 }
 
 export default MatPostgresGateway;

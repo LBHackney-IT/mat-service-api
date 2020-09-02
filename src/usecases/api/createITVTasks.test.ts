43d99d07ef5d3db016510d60193716f6cd2fe443
Ben Pirt <ben@pirt.co.uk>
2020-09-02 18:07:10 +0100

    Add unit tests for createITVTasks use case

--- src/usecases/api/createITVTasks.test.ts
+++ src/usecases/api/createITVTasks.test.ts
@@ -1,39 +1,64 @@
-import v1MatAPIGateway from '../../gateways/v1MatAPIGateway';
+import { TenancyManagementInteraction} from '../../interfaces/tenancyManagementInteraction';
 import MockTenancy from '../../tests/helpers/generateTenancy';
-import CreateITVTasks from './createITVTasks';
-
-jest.mock('../../gateways/v1MatAPIGateway');
+import CreateITVTasksUseCase from './createITVTasks';
+import {tenancyToITVTask} from '../../mappings/tenancyToITVTask';
 
 describe('createITVTasks', () => {
-  beforeEach(() => {
-    v1MatAPIGateway.mockClear();
-  });
-
   it('should retrieve new tenancies and create ITV tasks for them', async () => {
-    const getNewTenancies = jest.fn(() => ({
-      body: [MockTenancy(true), MockTenancy(true)],
+    const tenancies = [MockTenancy(true), MockTenancy(true)];
+    const getNewTenancies = jest.fn(async () => ({
+      body: tenancies,
       error: undefined,
     }));
 
-    const createTenancyManagementInteraction = jest.fn((tmi) => ({
+    const createTenancyManagementInteraction = jest.fn(async (tmi: TenancyManagementInteraction) => ({
       body: [],
       error: undefined,
     }));
 
-    v1MatAPIGateway.mockImplementation(() => {
-      return {
-        getNewTenancies,
-        createTenancyManagementInteraction,
-      };
-    });
+    const v1MatAPIGateway = {
+      getNewTenancies: <jest.Mock>(getNewTenancies),
+      createTenancyManagementInteraction: <jest.Mock>(createTenancyManagementInteraction),
+    };
 
-    const usecase = new CreateITVTasks();
-    usecase.execute();
+    const createITVTasks = new CreateITVTasksUseCase({
+      gateway: v1MatAPIGateway
+    })
 
+    await createITVTasks.execute();
+console.log(createTenancyManagementInteraction.mock.calls[0])
     expect(getNewTenancies).toHaveBeenCalledTimes(1);
-    console.log(createTenancyManagementInteraction.mock.instances);
     expect(createTenancyManagementInteraction).toHaveBeenCalledTimes(2);
+    expect(createTenancyManagementInteraction.mock.calls[0][0]).toEqual(tenancyToITVTask(tenancies[0]))
+    expect(createTenancyManagementInteraction.mock.calls[1][0]).toEqual(tenancyToITVTask(tenancies[1]))
   });
 
-  // it('should only create tasks for introductory tenancies', async () => {});
+  it('should only create tasks for introductory tenancies', async () => {
+    const tenancies = [MockTenancy(true), MockTenancy(false), MockTenancy(true)];
+    const getNewTenancies = jest.fn(async () => ({
+      body: tenancies,
+      error: undefined,
+    }));
+
+    const createTenancyManagementInteraction = jest.fn(async (tmi: TenancyManagementInteraction) => ({
+      body: [],
+      error: undefined,
+    }));
+
+    const v1MatAPIGateway = {
+      getNewTenancies: <jest.Mock>(getNewTenancies),
+      createTenancyManagementInteraction: <jest.Mock>(createTenancyManagementInteraction),
+    };
+
+    const createITVTasks = new CreateITVTasksUseCase({
+      gateway: v1MatAPIGateway
+    })
+
+    await createITVTasks.execute();
+
+    expect(getNewTenancies).toHaveBeenCalledTimes(1);
+    expect(createTenancyManagementInteraction).toHaveBeenCalledTimes(2);
+    expect(createTenancyManagementInteraction.mock.calls[0][0]).toEqual(tenancyToITVTask(tenancies[0]))
+    expect(createTenancyManagementInteraction.mock.calls[1][0]).toEqual(tenancyToITVTask(tenancies[2]))
+  });
 });
--- src/usecases/api/createITVTasks.ts
+++ src/usecases/api/createITVTasks.ts
@@ -1,23 +1,25 @@
-import v1MatAPIGateway, {
-  v1MatAPIGatewayInterface,
-} from '../../gateways/v1MatAPIGateway';
-import { Tenancy } from '../../interfaces/tenancy';
+import { v1MatAPIGatewayInterface } from '../../gateways/v1MatAPIGateway';
 import { TenancyManagementInteraction } from '../../interfaces/tenancyManagementInteraction';
+import {tenancyToITVTask} from '../../mappings/tenancyToITVTask';
 
 interface CreateITVTasksResponse {
   body: any | undefined;
   error: number | undefined;
 }
 
+interface CreateITVTasksOptions {
+  gateway: v1MatAPIGatewayInterface;
+}
+
 interface CreateITVTasksInterface {
   execute(): Promise<CreateITVTasksResponse>;
 }
 
-class CreateITVTasks implements CreateITVTasksInterface {
+class CreateITVTasksUseCase implements CreateITVTasksInterface {
   v1MatAPIGateway: v1MatAPIGatewayInterface;
 
-  constructor() {
-    this.v1MatAPIGateway = new v1MatAPIGateway();
+  constructor(options: CreateITVTasksOptions) {
+    this.v1MatAPIGateway = options.gateway;
   }
 
   public async execute(): Promise<CreateITVTasksResponse> {
@@ -52,60 +54,19 @@ class CreateITVTasks implements CreateITVTasksInterface {
 
     // Loop over the remaining introductory tenancies, convert to tmi objects
     const tmiObjects: TenancyManagementInteraction[] = introductoryTenancies.map(
-      (tenancy) => {
-        return {
-          transferred: undefined,
-          status: undefined,
-          parentInteractionId: undefined,
-          householdId: undefined,
-          processStage: undefined,
-          reasonForStartingProcess: undefined,
-          assignedToPatch: undefined,
-          assignedToManager: undefined,
-          managerId: undefined,
-          estateOffice: undefined,
-          adviceGiven: undefined,
-          interactionId: undefined,
-          contactId: tenancy.contacts[0].contactId,
-          enquirySubject: '100000060',
-          estateOfficerId: tenancy.officerId,
-          subject: 'c1f72d01-28dc-e711-8115-70106faa6a11',
-          source: '1',
-          natureofEnquiry: '15',
-          estateOfficerName: tenancy.officerName,
-          officerPatchId: tenancy.patchId,
-          areaName: tenancy.areaId,
-          serviceRequest: {
-            id: undefined,
-            title: 'Initial tenancy visit',
-            description: 'Starting an initial tenancy visit',
-            contactId: tenancy.contacts[0].contactId,
-            parentCaseId: undefined,
-            createdDate: undefined,
-            enquiryType: undefined,
-            ticketNumber: undefined,
-            requestCallback: undefined,
-            transferred: undefined,
-            createdBy: undefined,
-            childRequests: undefined,
-            subject: 'c1f72d01-28dc-e711-8115-70106faa6a11',
-            householdId: tenancy.householdId,
-          },
-          processType: 1,
-        };
-      }
+      (tenancy) => tenancyToITVTask(tenancy)
     );
 
     // Send to the api endpoint to create a tmi
-    // Which api... to AWS as a Lambda function...and then where to?
-    tmiObjects.forEach(async (tmi) => {
-      // console.log(
-      //   'CreateTenancyManagementInteraction call',
-      //   await this.v1MatAPIGateway.createTenancyManagementInteraction(tmi)
-      // );
+    for(const tmi of tmiObjects){
       await this.v1MatAPIGateway.createTenancyManagementInteraction(tmi);
-    });
+    };
+
+    return {
+      body: true,
+      error: undefined
+    }
   }
 }
 
-export default CreateITVTasks;
+export default CreateITVTasksUseCase;

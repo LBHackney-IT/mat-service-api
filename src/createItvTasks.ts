3771c681f6170ac5f563858c5e1ce4437f5b43f0
Ben Pirt <ben@pirt.co.uk>
2020-09-03 11:03:21 +0100

    Re-work the task entry point

--- src/createItvTasks.ts
+++ src/createItvTasks.ts
@@ -1,6 +1,16 @@
 import CreateITVTasks from './usecases/api/createITVTasks';
+import v1MatAPIGateway from './gateways/v1MatAPIGateway';
 
 module.exports = async () => {
-  const usecase = new CreateITVTasks();
+  if(!process.env.V1_MAT_API_URL || !process.env.V1_MAT_API_TOKEN){
+    throw(new Error("Missing environment variable for createItvTasks"))
+  };
+
+  const gateway: v1MatAPIGateway = new v1MatAPIGateway({
+    v1MatApiUrl: process.env.V1_MAT_API_URL,
+    v1MatApiToken: process.env.V1_MAT_API_TOKEN
+  })
+  const usecase = new CreateITVTasks({ gateway });
+
   await usecase.execute();
 };

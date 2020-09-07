4d1e1a629b22f5147938f822abcf61dea5d8b781
Ben Pirt <ben@pirt.co.uk>
2020-09-07 16:51:20 +0100

    Get the process for creating a manual task in place

---
+++ src/usecases/api/createManualTask.ts
@@ -0,0 +1,89 @@
+import { v1MatAPIGatewayInterface } from '../../gateways/v1MatAPIGateway';
+import { TenancyManagementInteraction } from '../../interfaces/tenancyManagementInteraction';
+import { tenancyToITVTask } from '../../mappings/tenancyToITVTask';
+
+interface TmiData {
+  title: string;
+  description: string;
+}
+
+const tmiLookup: { [key: string]: TmiData } = {
+  '100000052': { title: 'Home Check', description: 'Starting a home check' },
+  '100000060': {
+    title: 'Introductory Tenancy Visit',
+    description: 'Starting an introductory tenancy visit',
+  },
+  '100000156': {
+    title: 'Tenancy & Household Check',
+    description: 'Starting a tenancy & household check',
+  },
+};
+
+interface CreateManualTaskResponse {
+  body: any | undefined;
+  error: number | undefined;
+}
+
+interface CreateManualTaskOptions {
+  gateway: v1MatAPIGatewayInterface;
+}
+
+interface CreateManualTaskInterface {
+  execute(): Promise<CreateManualTaskResponse>;
+}
+
+interface CreateManualTaskData {
+  tagRef: string;
+  uprn: string;
+  process: string;
+  subProcess?: number;
+}
+
+class CreateManualTaskUseCase implements CreateManualTaskInterface {
+  v1MatAPIGateway: v1MatAPIGatewayInterface;
+
+  constructor(options: CreateManualTaskOptions) {
+    this.v1MatAPIGateway = options.gateway;
+  }
+
+  public async execute(
+    processData: CreateManualTaskData
+  ): Promise<CreateManualTaskResponse> {
+    const contact = await this.v1MatAPIGateway.getContactsByUprn(
+      processData.uprn
+    );
+
+    const tmi: TenancyManagementInteraction = {
+      enquirySubject: processData.process,
+      reasonForStartingProcess: processData.subProcess,
+      subject: 'c1f72d01-28dc-e711-8115-70106faa6a11',
+      natureofEnquiry: '15',
+      source: '1',
+      contactId: contact.contactId,
+      estateOfficerId: 'TBC',
+      estateOfficerName: 'TBC',
+      officerPatchId: 'TBC',
+      areaName: 0,
+      householdId: 'TBC',
+      serviceRequest: {
+        title: tmiLookup[processData.process].title,
+        description: tmiLookup[processData.process].description,
+        contactId: 'TBC',
+        subject: 'c1f72d01-28dc-e711-8115-70106faa6a11',
+        enquiryType: 'TBC',
+        createdBy: 'TBC',
+        childRequests: [],
+      },
+    };
+
+    // Send to the api endpoint to create a tmi
+    await this.v1MatAPIGateway.createTenancyManagementInteraction(tmi);
+
+    return {
+      body: true,
+      error: undefined,
+    };
+  }
+}
+
+export default CreateManualTaskUseCase;

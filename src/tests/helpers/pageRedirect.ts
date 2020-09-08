b30a3748d97ca4f4293336bea031f8e49b68a046
LBHABEBIC <ana.bebic@hackney.gov.uk>
2020-09-08 13:39:07 +0100

    Add simple Navbar to index page

---
+++ src/tests/helpers/pageRedirect.ts
@@ -0,0 +1,20 @@
+const pageRedirect = [
+  {
+    name: 'Home',
+    url: '/',
+  },
+  {
+    name: 'Residents',
+    url: '/residents',
+  },
+  {
+    name: 'Tenant and Resident Associations',
+    url: '/TRA',
+  },
+  {
+    name: 'Administration',
+    url: '/Administration',
+  },
+];
+
+export default pageRedirect;

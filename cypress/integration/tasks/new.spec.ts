abdf9c2771ed83a2737e44038786da7fb2f5f76f
Ben Pirt <ben@pirt.co.uk>
2020-09-08 09:43:04 +0100

    Add tests for new task page functionality

--- cypress/integration/tasks/new.spec.ts
+++ cypress/integration/tasks/new.spec.ts
@@ -6,7 +6,7 @@ import generateToken from '../../../src/tests/helpers/generateToken';
 const jwtSecret = Cypress.env('JWT_SECRET');
 
 describe('Tenancy Page Elements', () => {
-  it('', () => {
+  beforeEach(() => {
     let token = generateToken(
       '108854273331484808552',
       'Test User',
@@ -17,7 +17,38 @@ describe('Tenancy Page Elements', () => {
 
     cy.setCookie('hackneyToken', token);
     cy.visit('/tasks/new?tag_ref=1234567-1&uprn=12345678901');
+  });
 
+  it('enables the button if home check is selected', () => {
+    cy.get('input#homecheck').check();
+    cy.get('button').should('not.be.disabled');
+  });
+
+  it('enables the button if itv is selected', () => {
+    cy.get('input#itv').check();
+    cy.get('button').should('not.be.disabled');
+  });
+
+  it('displays a submenu with disabled button if thc is selected', () => {
+    cy.get('button').should('be.disabled');
+    cy.contains('Gas safety forced entry').should('not.exist');
+    cy.get('input#thc').check();
+    cy.get('button').should('be.disabled');
+    cy.contains('Gas safety forced entry');
+  });
+
+  it('only enables the button if the submenu is selected', () => {
+    cy.get('button').should('be.disabled');
+    cy.contains('Gas safety forced entry').should('not.exist');
+    cy.get('input#thc').check();
+    cy.get('button').should('be.disabled');
+    cy.contains('Gas safety forced entry');
+    cy.get('input#thcReason3').check();
+    cy.get('button').should('not.be.disabled');
+  });
+
+  it('displays the form with a disabled button', () => {
     cy.contains('Create New Process');
+    cy.get('button').should('be.disabled');
   });
 });

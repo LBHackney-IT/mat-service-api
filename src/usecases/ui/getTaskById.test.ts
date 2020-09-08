4316ff09ea9a7f43653b1cdfdbe70e5f2659c246
Christopher Parsons <mail@christopherparsons.me>
2020-09-08 14:53:14 +0100

    Add test for fetching tasks by ID

---
+++ src/usecases/ui/getTaskById.test.ts
@@ -0,0 +1,23 @@
+import getTaskById from "./getTaskById";
+import faker from "faker";
+import axios from "axios";
+import MockTask from "../../tests/helpers/generateTask";
+jest.mock("axios");
+
+describe("getTaskById", () => {
+  beforeEach(() => {
+    axios.mockClear();
+  })
+
+  it("can successfully fetch data from an API", async () => {
+    process.env.NEXT_PUBLIC_API_PATH = "http://localhost:3000/api"
+
+    const data = {data: MockTask()};
+
+    axios.get.mockResolvedValue(data);
+
+    const response  = await getTaskById(faker.lorem.word());
+
+    expect(response).toEqual(data.data);
+  })
+})

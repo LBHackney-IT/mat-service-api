b27155058907fad98ec703682033d66a99a66fb5
Christopher Parsons <mail@christopherparsons.me>
2020-09-08 14:54:52 +0100

    Add type to response

--- src/usecases/ui/getTaskById.ts
+++ src/usecases/ui/getTaskById.ts
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import { Task } from '../../interfaces/task';
 
-const getTaskById = async(taskId: string) => {
+const getTaskById = async(taskId: string): Promise<Task> => {
   const task = await axios
     .get(`${process.env.NEXT_PUBLIC_API_PATH}/tasks/${taskId}`)
     .then((response => {
       return response;
     }))
 
-  return task.data;
+  return task.data as Task;
 }
 
 export default getTaskById;

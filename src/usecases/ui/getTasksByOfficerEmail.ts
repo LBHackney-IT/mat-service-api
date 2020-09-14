1445bd9529aa1d99c770aa46807ca8b9efa32378
Christopher Parsons <mail@christopherparsons.me>
2020-09-14 15:53:53 +0100

    Remove commented code

--- src/usecases/ui/getTasksByOfficerEmail.ts
+++ src/usecases/ui/getTasksByOfficerEmail.ts
@@ -5,7 +5,6 @@ import getEmailAddress from './getEmailAddress';
 
 const getTasksByOfficerEmail = async (): Promise<Row[]> => {
   const emailAddress = getEmailAddress();
-  // const emailAddress = "tuomo.karki@hackney.gov.uk";
 
   if (
     process.env.NEXT_PUBLIC_API_PATH === undefined ||

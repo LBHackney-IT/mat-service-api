d587e56ae7ecca8a7da5d1a866ff7be7a693ab27
Ben Pirt <ben@pirt.co.uk>
2020-09-15 08:21:18 +0100

    Add some error handling to the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Worktray, { sampleWorkTrayColumns, Row } from '../components/worktray';
+import { ErrorMessage } from 'lbh-frontend-react';
 import Layout from '../components/layout';
 import getTasksByOfficerEmail from '../usecases/ui/getTasksByOfficerEmail';
 
+type FetchState = 'fetching' | 'error' | 'done';
+
 export default function Home() {
   const [tasks, setTasks] = useState<Row[]>([]);
+  const [fetchState, setFetchState]: [FetchState, any] = useState('fetching');
 
   useEffect(() => {
-    const fetchTasks = async () => {
-      const fetchedTasks = await getTasksByOfficerEmail();
-      setTasks(fetchedTasks);
-    };
-    fetchTasks();
+    getTasksByOfficerEmail()
+      .then((tasks) => {
+        setTasks(tasks);
+        setFetchState('done');
+      })
+      .catch((e) => {
+        setFetchState('error');
+      });
   }, []);
 
   return (
@@ -24,10 +31,25 @@ export default function Home() {
             <link rel="icon" href="/favicon.ico" />
           </Head>
           <main>
-            <Worktray columns={sampleWorkTrayColumns} rows={tasks} />
+            {
+              {
+                fetching: <p className="message">Fetching worktray items</p>,
+                error: (
+                  <ErrorMessage>Error fetching worktray items</ErrorMessage>
+                ),
+                done: <Worktray columns={sampleWorkTrayColumns} rows={tasks} />,
+              }[fetchState]
+            }
           </main>
         </div>
       </div>
+
+      <style jsx>{`
+        .message {
+          font-weight: 700;
+          font-size: 1.1875rem;
+        }
+      `}</style>
     </Layout>
   );
 }

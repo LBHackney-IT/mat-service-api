399ced0d137a8915fd5f99569a39704748331b4e
Ben Pirt <ben@pirt.co.uk>
2020-09-15 16:12:08 +0100

    Refactor the setting up of user mapping to happen via the initial call to /api/tasks
    It wasn't working on staging (the getInitialProps didn't seem to be running properly)
    also this means that we only set up the user mapping after a successful login
    rather than calling it on every page.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,7 @@ import cookie from 'cookie';
 import App, { AppProps, AppContext } from 'next/app';
 import isLoggedIn from '../usecases/ui/isLoggedIn';
 import '../global.scss';
-import userMappingExists from '../usecases/ui/userMappingExists';
-import getCrmUserGuid from '../usecases/ui/getCrmUserGuid';
-import jwt from 'jsonwebtoken';
-import createUserMapping, {
-  UserMapping,
-} from '../usecases/ui/createUserMapping';
-import createCrmUser from '../usecases/ui/createCrmUser';
+import { ServerResponse } from 'http';
 
 const unauthenticatedLandingPage = '/login-redirect';
 
@@ -17,89 +11,36 @@ function MaTApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-MaTApp.getInitialProps = async (context: AppContext) => {
-  console.log('Calling GetInitialProps on main app');
-  const appProps = App.getInitialProps(context);
-  try {
-    if (
-      context.ctx &&
-      context.ctx.req &&
-      context.ctx.req.headers &&
-      context.ctx.req.headers.cookie
-    ) {
-      let parsedCookie = cookie.parse(context.ctx.req.headers.cookie);
+const redirect = (res: ServerResponse) => {
+  res.writeHead(302, { Location: unauthenticatedLandingPage });
+  res.end();
+};
 
-      if (
-        parsedCookie &&
-        parsedCookie.hackneyToken &&
-        isLoggedIn(parsedCookie.hackneyToken) === true
-      ) {
-        console.log('Calling userMappingManagement from GetInitialProps');
-        userMappingManagement(parsedCookie);
-        return { ...appProps };
-      }
+MaTApp.getInitialProps = async (context: AppContext) => {
+  const appProps = App.getInitialProps(context);
+
+  if (!context.ctx || !context.ctx.req || !context.ctx.res) {
+    return { ...appProps };
+  }
+
+  if (context.ctx.req.headers && context.ctx.req.headers.cookie) {
+    let parsedCookie = cookie.parse(context.ctx.req.headers.cookie);
+
+    if (!parsedCookie || !parsedCookie.hackneyToken) {
+      redirect(context.ctx.res);
     }
 
     if (
-      context.ctx &&
-      context.ctx.res &&
       context.ctx.pathname !== unauthenticatedLandingPage &&
-      !context.ctx.pathname.startsWith('/api')
+      !context.ctx.pathname.startsWith('/api') &&
+      !isLoggedIn(parsedCookie.hackneyToken)
     ) {
-      console.log('No valid token - redirecting from ' + context.ctx.pathname);
-      context.ctx.res.writeHead(302, { Location: unauthenticatedLandingPage });
-      context.ctx.res.end();
-      return { ...appProps };
+      redirect(context.ctx.res);
     } else {
       return { ...appProps };
     }
-  } catch (e) {
-    console.log('Error in GetInitialProps', e);
   }
+  redirect(context.ctx.res);
 };
 
 export default MaTApp;
-
-interface HackneyToken {
-  sub: string;
-  email: string;
-  iss: string;
-  name: string;
-  groups: string[];
-  iat: number;
-}
-
-const userMappingManagement = async (parsedCookie: {
-  [key: string]: string;
-}) => {
-  console.log('Calling userMappingManagement on main app');
-  const hackneyToken = jwt.decode(parsedCookie.hackneyToken) as HackneyToken;
-  if (hackneyToken.email) {
-    const existingUserMapping = await userMappingExists(hackneyToken.email);
-    if (existingUserMapping) {
-      console.log('userMappingManagement: user already exists');
-      return;
-    } else {
-      const crmUserGuid = await getCrmUserGuid(hackneyToken.email);
-      console.log('crmUserGuid:', crmUserGuid);
-      let crmId;
-      if (crmUserGuid === undefined) {
-        const splitName = hackneyToken.name.split(' ');
-        const user = {
-          fullName: hackneyToken.name,
-          firstName: splitName[0],
-          familyName: splitName[splitName.length - 1],
-          emailAddress: hackneyToken.email,
-        };
-        crmId = await createCrmUser(user);
-      }
-      const userMapping: UserMapping = {
-        name: hackneyToken.name,
-        emailAddress: hackneyToken.email,
-        googleId: hackneyToken.iat.toString(),
-        usercrmid: crmUserGuid ? crmUserGuid.data : crmId,
-      };
-      await createUserMapping(userMapping);
-    }
-  }
-};

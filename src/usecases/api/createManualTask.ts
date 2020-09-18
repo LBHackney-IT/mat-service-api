bcd64306fc8eb110b11b9d9b0f34b7ebe3d76dbe
Ben Pirt <ben@pirt.co.uk>
2020-09-18 14:02:09 +0100

    Fix failing tests

--- src/usecases/api/createManualTask.ts
+++ src/usecases/api/createManualTask.ts
@@ -64,13 +64,13 @@ class CreateManualTaskUseCase implements CreateManualTaskInterface {
     const contacts = await this.v1MatAPIGateway.getContactsByUprn(
       processData.uprn
     );
-    if (contacts.error) return contacts as CreateManualTaskResponse;
     if (
+      contacts.error ||
       (contacts && contacts.body && contacts.body.length === 0) ||
       !contacts ||
       !contacts.body
     ) {
-      return { error: 404, body: undefined };
+      return { error: 'Error fetching contacts', body: undefined };
     }
 
     const contact = contacts.body[0];
@@ -82,41 +82,41 @@ class CreateManualTaskUseCase implements CreateManualTaskInterface {
       crmGateway,
       matPostgresGateway,
     });
+
     const officerDetails = await getOfficerPatchId.execute();
-    if (!officerDetails.body || officerDetails.error)
-      return { error: 404, body: undefined };
+    if (
+      !officerDetails.body ||
+      officerDetails.error ||
+      !officerDetails.body.areaId ||
+      !officerDetails.body.patchId
+    ) {
+      return { error: 'Error fetching officer patch details', body: undefined };
+    }
 
     let tmi: TenancyManagementInteraction;
 
-    if (
-      officerDetails.body.areaId === undefined ||
-      officerDetails.body.patchId === undefined
-    ) {
-      return { error: 400, body: undefined };
-    } else {
-      tmi = {
-        enquirySubject: tmiLookup[processData.process].enquirySubject,
-        reasonForStartingProcess: processData.subProcess,
-        subject: 'c1f72d01-28dc-e711-8115-70106faa6a11',
-        natureofEnquiry: '15',
-        source: '1',
+    tmi = {
+      enquirySubject: tmiLookup[processData.process].enquirySubject,
+      reasonForStartingProcess: processData.subProcess,
+      subject: 'c1f72d01-28dc-e711-8115-70106faa6a11',
+      natureofEnquiry: '15',
+      source: '1',
+      contactId: contact.contactId,
+      estateOfficerId: officerDetails.body.officerId,
+      estateOfficerName: processData.officerName,
+      officerPatchId: officerDetails.body.patchId,
+      areaName: officerDetails.body.areaId,
+      householdId: contact.houseRef,
+      processType: 1,
+      serviceRequest: {
+        title: tmiLookup[processData.process].title,
+        description: tmiLookup[processData.process].description,
         contactId: contact.contactId,
-        estateOfficerId: officerDetails.body.officerId,
-        estateOfficerName: processData.officerName,
-        officerPatchId: officerDetails.body.patchId,
-        areaName: officerDetails.body.areaId,
-        householdId: contact.houseRef,
-        processType: 1,
-        serviceRequest: {
-          title: tmiLookup[processData.process].title,
-          description: tmiLookup[processData.process].description,
-          contactId: contact.contactId,
-          subject: 'c1f72d01-28dc-e711-8115-70106faa6a11',
-          createdBy: officerDetails.body.officerId,
-          childRequests: [],
-        },
-      };
-    }
+        subject: 'c1f72d01-28dc-e711-8115-70106faa6a11',
+        createdBy: officerDetails.body.officerId,
+        childRequests: [],
+      },
+    };
     // Send to the api endpoint to create a tmi
     return await this.v1MatAPIGateway.createTenancyManagementInteraction(tmi);
   }

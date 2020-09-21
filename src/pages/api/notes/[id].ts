a6692b1a9bee39e081ec764d5af72a83fdee5c29
Christopher Parsons <mail@christopherparsons.me>
2020-09-21 10:51:22 +0100

    Add controller for the new notes endpoint

---
+++ src/pages/api/notes/[id].ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { ApiNote } from '../../../interfaces/note';
+import GetNotesForTask from '../../../usecases/api/getNotesForTask';
+
+interface Error {
+  error: string;
+}
+
+type Data = ApiNote[] | Error;
+
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const id = req.query.id
+    ? Array.isArray(req.query.id)
+      ? req.query.id[0]
+      : req.query.id
+    : undefined;
+
+  if (id !== undefined) {
+    const getNotesForTask = new GetNotesForTask(id);
+    const response = await getNotesForTask.execute();
+
+    if (response.body) {
+      res.status(200).json(response.body);
+    } else {
+      res
+        .status(response.error || 500)
+        .json({ error: 'could not retrieve notes' });
+    }
+  } else {
+    res.status(400).json({ error: 'task id missing' });
+  }
+};

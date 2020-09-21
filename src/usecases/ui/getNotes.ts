5c41188dcc5a4512057fabb83dfa3ac8877137b0
Christopher Parsons <mail@christopherparsons.me>
2020-09-21 11:13:39 +0100

    Add ui getNotes usecase

---
+++ src/usecases/ui/getNotes.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { ApiNote } from '../../interfaces/note';
+
+const getNotesById = async (taskId: string): Promise<ApiNote[]> => {
+  if (process.env.NEXT_PUBLIC_API_PATH === undefined) {
+    return [];
+  }
+
+  const response = await axios
+    .get(`${process.env.NEXT_PUBLIC_API_PATH}/notes/${taskId}`)
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      return [];
+    });
+
+  if (response !== undefined) {
+    return response as ApiNote[];
+  }
+  return response;
+};
+
+export default getNotesById;
